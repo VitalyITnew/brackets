211ee1023af3933244eb20bb7c9dae3ead685e75
VitalyItnew <103492966+VitalyITnew@users.noreply.github.com>
2022-07-28 16:42:30 +0300

    feat:add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let pairs = bracketsConfig.map(arr => arr.join('')); 
+//создаем пары вида () из массивов "скобок"
+  
+  while (pairs.length) {
+    let i = 0;
+
+    for (let item of pairs) {
+      if (str.indexOf(item) > -1) {
+        str = str.replace(item, '');
+      } else {i++}
+    }
+
+    if (i === pairs.length) pairs.length = 0;  
+  }
+//если в строке есть любая пара из pairs, то "удаляем" её, заменяя на ''. И так в цикле пока не продём все пары
+
+  return (!str) ? true : false;
+//если после всех манипуляций длина строки равна 0, то всё верно, если нет, то ложно
+} 
